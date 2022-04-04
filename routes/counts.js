cde09e8a38c80a8ebff3937fc5f8a672dbc35da1
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-04 18:34:46 +0200

    test

--- routes/counts.js
+++ routes/counts.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res)=>{
 })
 router.get('/:id', async (req, res)=>{
    try {
-       const counts  = await Count.find(req.params.id)
+       const counts  = await Count.find(req.params._id)
        res.send(counts)
    } catch (error) {
        res.send("error ocurred")
