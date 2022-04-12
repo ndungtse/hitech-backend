d2bae6f01a0a89d4c9fc2b14a6a22bdf1189a644
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-12 15:37:30 +0200

    bug

--- app.js
+++ app.js
@@ -8,23 +8,6 @@ const cors = require("cors");
 
 app.use(bodyParser.json());
 
-/* var whitelist = [
-  "https://hitech1.herokuapp.com",
-  "http://localhost:4040",
-  "http://localhost:8080",
-  "https://hitech1.vercel.app/",
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-app.use(cors(corsOptions)); */
-
 app.use(cors({
   origin: "*",
   methods: "GET, POST, PUT"
