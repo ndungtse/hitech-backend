574a77068a2eac6a3e1d409f12abdb9a287a508f
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-28 11:29:57 +0200

    great_Ch

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv/config");
-
+// const fileUpload = multer();
+// const cloudinary = require("cloudinary").v2;
+// const streamifier = require("streamifier");
 
 const cors = require("cors");
 
@@ -24,6 +26,29 @@ app.get("/", (req, res) => {
   res.send("Welcome to hitech-backend!!!");
 });
 
+
+// app.post("/upload", fileUpload.single("image"), function (req, res, next) {
+//   let streamUpload = (req) => {
+//     return new Promise((resolve, reject) => {
+//       let stream = cloudinary.uploader.upload_stream((error, result) => {
+//         if (result) {
+//           resolve(result);
+//         } else {
+//           reject(error);
+//         }
+//       });
+
+//       streamifier.createReadStream(req.file.buffer).pipe(stream);
+//     });
+//   };
+
+//   async function upload(req) {
+//     let result = await streamUpload(req);
+//     console.log(result);
+//   }
+
+//   upload(req);
+// });
 
 const PORT = process.env.PORT || 8080;
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- models/userModel.js
+++ models/userModel.js
@@ -5,9 +5,30 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  picture: {
+    type: String,
+    default: 'https://img.icons8.com/ios-glyphs/60/user--v1.png'
+  },
+  messages: {
+    type: Array
+  },
+  role:{
+    type: String,
+  },
+  orders: {
+    type: Array
+  },
   email: {
     type: String,
     required: true,
+  },
+  specifics: {
+    products: {
+      type: Array,
+    },
+    counts: {
+      type: Object,
+    },
   },
   password: {
     type: String,
