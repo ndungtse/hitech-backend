103504de342590ddeb151bfe2cb9fea69fa1da03
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-29 09:26:27 +0200

    ra

--- valids/validation.js
+++ valids/validation.js
@@ -17,5 +17,16 @@ const logvalidation = (data)=>{
     return schema.validate(data)
 }
 
+const updateValidation = (data)=>{
+  const schema = Joi.object({
+    username: Joi.string(),
+    password: Joi.string().min(6),
+    role: Joi.string(),
+    picture: Joi.string(),
+  })
+  return schema.validate(data)
+}
+
 module.exports.regvalidation = regvalidation
 module.exports.logvalidation = logvalidation
+module.exports.updateValidation = updateValidation
